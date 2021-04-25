ef19fef99d3a5a2dfad3e050eaa6c45da692f4fb
aaronshivers <42848750+aaronshivers@users.noreply.github.com>
2021-04-25 07:59:17 -0500

    first commit

---
+++ src/App.test.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+
+describe('App', () => {
+  const app = shallow(<App />);
+
+  it('should render correctly', () => {
+    expect(app).toMatchSnapshot();
+  });
+
+  it('should have the text `Hello!!!`', () => {
+    expect(app.text()).toBe('Hello!!!');
+  });
+});
